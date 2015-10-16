d3edae00d13f121f73244a46f6b4e65f57e5bb55
Ashvala Vinay <mail@ashvala.net>
2015-10-16 14:49:16 -0400

    fix some dumb mistake I made

--- LineChain.py
+++ LineChain.py
@@ -1,15 +1,20 @@
 class Interp_text:
     def init(str):
         self.str_to_parse = str
-        
+
     def parser(self,str):
-        arr_str = tokenize(str)
+        arr_str = self.tokenize(str)
         return str
-        
+
     def tokenize(self, str):
-        return str.split("->")
-        
-        
-        
+        arr_str1 = str.split("->") #split signal chain direction
+        for item in arr_str1:
+            print item
+        return arr_str1
+
+
+
+
 string = "(midi)->(osc)->(adsr)-<[{->(stereo1), {->(reverbsc)->(stereo2)}]"
 text_instance = Interp_text()
+print text_instance.parser(string)
