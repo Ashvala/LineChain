25036d2c1c3dbfe2872659c079cfd96ff44d14a5
Ashvala Vinay <mail@ashvala.net>
2016-01-06 20:30:03 -0500

    Start working on unit generators

---
+++ DSP/LineChainUnits.py
@@ -0,0 +1,44 @@
+import json
+import LineChainBase
+
+class Ugens(LineChainBase.LineChain):
+    jlist = [
+        {
+            "name": "oscil",
+            "args": "float,float",
+            "args_count": 2,
+            "args_list": "amp, freq",
+            "unit_desc": "A typical oscillator"
+        },
+        {
+            "name": "adsr",
+            "args": "float,float,float,float",
+            "args_count": 4,
+            "args_list": "attack,decay,sustain,release",
+            "unit_desc": "Standard ADSR"
+        },
+        {
+            "name": "stereo1",
+            "args": "",
+            "args_count": 0,
+            "args_list": "",
+            "unit_desc": "Stereo channel 1"
+        },
+        {
+            "name": "stereo2",
+            "args": "",
+            "args_count": 0,
+            "args_list": "",
+            "unit_desc": "Stereo channel 2"
+        },
+        {
+            "name": "mono",
+            "args": "",
+            "args_count": 0,
+            "args_list": "",
+            "unit_desc": "mono"
+        }
+    ]
+    def return_ugens():
+        for item in jlist:
+            print json.dumps(item)
